f06095a4f738b7949ae700c18867a7a62e3bcbb2
Hari2644 <152772370+Hari2644@users.noreply.github.com>
2025-03-01 10:45:35 +0530

    Update Christian_Harper.py

--- Christian_Harper.py
+++ Christian_Harper.py
@@ -58,7 +58,7 @@ def load_data():
 
 df, faiss_index = load_data()
 
-st.markdown('<h1 class="chat-font">🤖 Nirmal Gaud Clone Chatbot</h1>', unsafe_allow_html=True)
+st.markdown('<h1 class="chat-font">🤖 Harper Clone Chatbot</h1>', unsafe_allow_html=True)
 st.markdown('<h3 class="chat-font">Ask me anything, and I\'ll respond as Nirmal Gaud!</h3>', unsafe_allow_html=True)
 st.markdown("---")
 
